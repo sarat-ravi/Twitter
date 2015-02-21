1276f7030abd24ffa59fb69f6c8607b2a2de9c56
Sarat Tallamraju <sarattallamraju@gmail.com>
2015-02-20 21:04:35 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Sarat Tallamraju on 2/20/15.
+//  Copyright (c) 2015 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Sarat Tallamraju on 2/20/15.
+//  Copyright (c) 2015 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
