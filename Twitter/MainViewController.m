9c77a5ee3bc498ac046f29b9f4b5d8e8bbd124d1
Sarat Tallamraju <sarattallamraju@gmail.com>
2015-02-28 21:35:34 -0800

    panning

--- Twitter/MainViewController.m
+++ Twitter/MainViewController.m
@@ -10,10 +10,16 @@
 #import "TweetsViewController.h"
 
 @interface MainViewController () <TweetsViewControllerDelegate>
+- (IBAction)onPanContentView:(UIPanGestureRecognizer *)sender;
 
 @property (nonatomic, strong) UIViewController *rootViewController;
 @property (nonatomic, strong) UIViewController *menuViewController;
 @property (strong, nonatomic) IBOutlet UIView *contentView;
+// @property (strong, nonatomic) IBOutlet UIView *backgroundContentView;
+@property (nonatomic, assign) BOOL hamburgerMenuOpened;
+@property (nonatomic, assign) NSInteger slideOffset;
+
+@property (nonatomic, assign) CGPoint rootCenter;
 
 @end
 
@@ -26,6 +32,7 @@
     if (self) {
         self.rootViewController = rootViewController;
         self.menuViewController = menuViewController;
+        self.slideOffset = 200;
         
         [[NSNotificationCenter defaultCenter] addObserverForName: @"Hamburger" object:nil queue: [NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
             [self onHamburgerButtonPressed];
@@ -39,12 +46,39 @@
 
 - (void)onHamburgerButtonPressed {
     NSLog(@"onHamburgerButtonPressed");
+    if (self.hamburgerMenuOpened) {
+        [self closeHamburgerMenu];
+    } else {
+        [self openHamburgerMenu];
+    }
+}
+
+- (void) openHamburgerMenu {
+    self.hamburgerMenuOpened = YES;
+    [UIView animateWithDuration: 1 delay: 0.1 usingSpringWithDamping: 0.6 initialSpringVelocity:0.9 options:0 animations:^{
+        // Animation
+        CGPoint center = self.rootViewController.view.center;
+        self.rootViewController.view.center = CGPointMake(self.rootCenter.x + self.slideOffset, self.rootCenter.y);
+    } completion:^(BOOL finished) {
+        // Completion
+    }];
+}
+
+- (void) closeHamburgerMenu {
+    self.hamburgerMenuOpened = NO;
+    [UIView animateWithDuration: 1 delay: 0.1 usingSpringWithDamping: 0.6 initialSpringVelocity:0.9 options:0 animations:^{
+        // Animation
+        CGPoint center = self.rootViewController.view.center;
+        self.rootViewController.view.center = CGPointMake(self.rootCenter.x, self.rootCenter.y);
+    } completion:^(BOOL finished) {
+    }];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // [self.contentView addSubview: self.menuViewController.view];
+    self.rootCenter = self.rootViewController.view.center;
+    [self.contentView addSubview: self.menuViewController.view];
     self.rootViewController.view.frame = self.contentView.frame;
     self.menuViewController.view.frame = self.contentView.frame;
     [self.contentView addSubview: self.rootViewController.view];
@@ -55,6 +89,26 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (IBAction)onPanContentView:(UIPanGestureRecognizer *)sender {
+    CGPoint delta = [sender translationInView: self.view];
+    CGFloat displacement = MIN(delta.x, self.slideOffset);
+    displacement = MAX(0, displacement);
+    
+    if ([sender state] == UIGestureRecognizerStateBegan) {
+        
+    } else if ([sender state] == UIGestureRecognizerStateChanged) {
+        CGPoint center = self.rootViewController.view.center;
+        self.rootViewController.view.center = CGPointMake(self.rootCenter.x + displacement, self.rootCenter.y);
+    } else if ([sender state] == UIGestureRecognizerStateEnded) {
+        if (ABS(displacement) >= self.slideOffset / 2.0) {
+            [self openHamburgerMenu];
+        } else {
+            // [self closeHamburgerMenu];
+        }
+    }
+    
 }
 
 @end
